dca1418b1ac4f2b9e21fef7e13473cdcfbb61bf3
Crussus1719 <90951557+Crussus1719@users.noreply.github.com>
2026-04-03 08:53:51 -0300

    feat: ultimate premium experience package 🚀💎🔥

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://premium-blog.netlify.app',
+  site: 'https://to2343.netlify.app',
   integrations: [
     tailwind({
       applyBaseStyles: false,
